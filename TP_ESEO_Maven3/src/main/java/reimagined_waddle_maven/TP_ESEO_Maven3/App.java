ac50d27b00a78f0c5fed86da349808c8bee2d67c
Pierre P <pierre.pavlovic@reseau.eseo.fr>
2018-11-14 09:43:54 +0400

    test commit

---
+++ TP_ESEO_Maven3/src/main/java/reimagined_waddle_maven/TP_ESEO_Maven3/App.java
@@ -0,0 +1,13 @@
+package reimagined_waddle_maven.TP_ESEO_Maven3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
